ff5fbcbd9e5a59b227c4d8e066d95ab1cc0652d7
Martes <144176559+I-Martes-I@users.noreply.github.com>
2026-07-06 01:09:20 -0400

    New commit

--- github-activity.py
+++ github-activity.py
@@ -1,20 +1,23 @@
 import sys
 import urllib.request
+import json
 
 def get_user_info(username):
     url = f"https://api.github.com/users/{username}/events"
 
     try:
         with urllib.request.urlopen(url) as response:
-            status_code = response.getcode()
-            print(f"Success! Status code: {status_code}")
+            return json.loads(response.read())
     except Exception as err:
         print(f"An error occurred: {err}")
-#return response.read()
+        return None
 
 args = sys.argv
 username = args[1]
 user_info = get_user_info(username)
 
+
+#print(json.dumps(user_info[0], indent=4))
 if user_info:
-    print("")
+    for event in user_info:
+        print(event["type"])
